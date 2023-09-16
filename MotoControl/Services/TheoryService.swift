715c15ba4cbd5610420e1ae1d84f6129a77f8530
Alex Zhulai <Lepiozdyx@gmail.com>
2023-09-16 15:56:31 -0300

    A refactor of the classes and structures responsible for the Model, Data and Persistence layers. Separation of Responsibilities.

---
+++ MotoControl/Services/TheoryService.swift
@@ -0,0 +1,67 @@
+//
+//  TheoryService.swift
+//  MotoControl
+//
+//  Created by Alex on 16.09.2023.
+//
+
+import CoreData
+
+final class TheoryService {
+    
+    private let persistenceController: PersistenceController
+    
+    init(persistenceController: PersistenceController) {
+        self.persistenceController = persistenceController
+    }
+    
+    // MARK: CRUD
+    func createTheory(with model: TheoryModel) {
+        let newTheory = Theory(context: persistenceController.container.viewContext)
+        newTheory.title = model.title
+        newTheory.definition = model.definition
+        newTheory.videoURL = model.videoURL
+        newTheory.image = model.image
+        
+        saveContext()
+    }
+    
+    func fetchTheories() -> [TheoryModel] {
+        let request: NSFetchRequest<Theory> = Theory.fetchRequest()
+        
+        do {
+            let fetchedTheory = try persistenceController.container.viewContext.fetch(request)
+            return fetchedTheory.map {
+                TheoryModel(title: $0.title ?? "", definition: $0.definition ?? "", videoURL: $0.videoURL, image: $0.image)
+            }
+        } catch {
+            print("Failed to fetch theories: \(error)")
+            return []
+        }
+    }
+    
+    func updateTheory(_ theory: Theory, with model: TheoryModel) {
+        theory.title = model.title
+        theory.definition = model.definition
+        theory.videoURL = model.videoURL
+        theory.image = model.image
+        
+        saveContext()
+    }
+    
+    func deleteTheory(_ theory: Theory) {
+        persistenceController.container.viewContext.delete(theory)
+        saveContext()
+    }
+    
+    private func saveContext() {
+        if persistenceController.container.viewContext.hasChanges {
+            do {
+                try persistenceController.container.viewContext.save()
+            } catch {
+                let nsError = error as NSError
+                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+            }
+        }
+    }
+}
